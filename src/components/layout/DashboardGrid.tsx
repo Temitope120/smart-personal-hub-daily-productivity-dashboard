6f356730ae47d0cb038010eebc3ab9c113e527a3
Adedoyin <doyinfummi2012@gmail.com>
2025-12-03 18:20:18 +0000

    static page layout - 40%

---
+++ src/components/layout/DashboardGrid.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashboardGrid = () => {
+  return (
+    <div>
+      Dashboard grid would be rendered by Dashboard component. it contains individual components of KIP, Analytics, ReminderCard, RecentNotesCard, TaskStatuscard, HabitProgressCricle, TimeTracker
+    </div>
+  )
+}
+
+export default dashboardGrid
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DashboardGrid from '../components/layout/DashboardGrid'
+
+const Dashboard = () => {
+    return (
+        <>
+        <h2 className='text-3xl'>
+            Dashboard
+        </h2>
+        <DashboardGrid />
+        </>
+    )
+}
+
+export default Dashboard
