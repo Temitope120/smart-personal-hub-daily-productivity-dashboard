2033e47b8226df7e0aa6a98a81613b13df968b69
Adedoyin <doyinfummi2012@gmail.com>
2025-12-07 19:48:52 +0000

    mobile responsiveness-60%, Dashboard-50%

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -7,13 +7,13 @@ interface ChildrenProps {
 
 const AppLayout = ({children}: ChildrenProps) => {
     return (
-        <div className="px-6 py-8 min-h-screen rounded-4xl flex items-start gap-6">
-            <aside className="w-[240px] fixed top-0 left-0">
+        <div className=" px-0 lg:px-6 py-8 min-h-screen rounded-4xl flex items-start gap-6">
+            <aside className="w-[240px] hidden md:block fixed top-0 left-0">
                 <Sidebar />
             </aside>
 
 
-            <main className='w-full ml-[240px] p-4 overflow-y-auto bg-[#F7F7F7] h-screen rounded-2xl md:rounded-3xl'>
+            <main className='w-full md:ml-[240px] p-4 overflow-y-auto bg-white h-screen rounded-2xl md:rounded-3xl'>
               {children}
             </main>
 
