bbcf556c5793f77600ca4828df03423606b429e9
Adedoyin <doyinfummi2012@gmail.com>
2026-01-01 23:54:00 +0000

    Navigation fix: fixed sidebar click blockage

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,23 +1,18 @@
-// import React, { type ReactNode } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router'
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
-
-// interface ChildrenProps {
-//     children: ReactNode
-// }
 
 const AppLayout = () => {
     return (
         <>
             <Topbar />
             <div className=" px-0 lg:px-6 py-8 min-h-screen rounded-4xl flex items-start gap-6">
-                <aside className="w-[240px] hidden lg:block fixed top-0 left-0 -z-10">
+                <aside className="w-[240px] hidden lg:block fixed top-0 left-0 z-10">
                     <Sidebar />
                 </aside>
 
 
-                <main className='w-full lg:ml-[240px] p-4 overflow-y-auto bg-white h-screen rounded-2xl md:rounded-3xl'>
+                <main className='w-full lg:ml-[240px] p-4 overflow-y-auto bg-white h-screen rounded-2xl md:rounded-3xl xl:mt-[50px]'>
                     <Outlet />
                 </main>
 
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const Tasks = () => {
   return (
-    <div>
+    <div className="">
       Task here
     </div>
   )
